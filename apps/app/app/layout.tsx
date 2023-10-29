04d42f48fbe22239c2b05681bf28b219324aebea
christopherwxyz <chrstphrwllc@gmail.com>
2023-10-29 15:15:20 -0400

    rebuild

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,22 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-// Import necessary libraries and components
-import { WagmiConfig, createConfig, mainnet } from 'wagmi';
-import { createPublicClient, http } from 'viem';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-// Configure wagmi
-const config = createConfig({
-  autoConnect: true,
-  publicClient: createPublicClient({
-    chain: mainnet,
-    transport: http()
-  }),
-});
-
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -29,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <WagmiConfig config={config}>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-    </WagmiConfig>
   )
 }
