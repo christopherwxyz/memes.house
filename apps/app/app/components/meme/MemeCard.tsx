c97f4abf32472b15bf9b44aff23e35f678653430
christopherwxyz <chrstphrwllc@gmail.com>
2023-11-06 08:52:04 -0500

    Update MemeCard component with @radix-ui Flex and Text components

--- apps/app/app/components/meme/MemeCard.tsx
+++ apps/app/app/components/meme/MemeCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Avatar, Badge, Box, Container, Em, Separator, Text } from "@radix-ui/themes";
+import { Avatar, Badge, Box, Container, Em, Flex, Separator, Text } from "@radix-ui/themes";
 import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 
 
@@ -16,24 +16,27 @@ export function MemeCard() {
             objectFit="cover"
           />
           <Separator my="3" size="4" />
-          <Box
-            style={{ backgroundColor: 'var(--dark)', borderRadius: 'var(--radius-3)', borderColor: 'var(--gray)' }}>
-            <Text>
-              <Avatar size={"2"} src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop" fallback="MH" />
+          <Flex justify="between" gap={"3"} direction={"row"}>
+            <Box
+              style={{ backgroundColor: 'var(--dark)', borderRadius: 'var(--radius-3)', borderColor: 'var(--gray)' }}>
+              <Text>
+                <Avatar size={"2"} src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop" fallback="MH" />
+                <Text size="2" weight="regular" className="pl-3">
+                  <Em>
+                    @christopherw.eth
+                  </Em>
+                </Text>
+              </Text>
+            </Box>
+            <Box right="auto">
               <Badge className="m-2" color="green">
-                <Text size="1" weight="bold">
+                <Text size="2" weight="bold">
                   0.1 ETH
                 </Text>
               </Badge>
-              <Text size="2" weight="regular">
-                <Em>
-                by @christopher
-                </Em>
-              </Text>
-            </Text>
-          </Box>
+            </Box>
+          </Flex>
         </Container>
-
       </Box>
     </div>
   );
